8183ef5036ffd249defb692efdbd80049eea7407
Where are you going? <49277976+jgyy4775@users.noreply.github.com>
2021-12-21 23:45:25 +0900

    Create 43105.py

---
+++ code/43105.py
@@ -0,0 +1,8 @@
+def solution(triangle):
+    for i in range(1, len(triangle)):
+        for j in range(len(triangle[i])):
+            if j==0: triangle[i][j]=triangle[i-1][0]+triangle[i][j]
+            elif j==len(triangle[i])-1: triangle[i][j]=triangle[i-1][j-1]+triangle[i][j]
+            else: triangle[i][j]=max(triangle[i-1][j-1],triangle[i-1][j])+triangle[i][j]
+
+    return max(triangle[-1])
