2a11503344da650cdb9d34fc83f14e730699a96e
Where are you going? <49277976+jgyy4775@users.noreply.github.com>
2021-12-25 21:27:01 +0900

    Create 12914.py

---
+++ 12914.py
@@ -0,0 +1,7 @@
+def solution(n):
+    dp=[0]*(n+1)
+    for i in range(1, n+1):
+        if i==1: dp[i]=1
+        elif i==2: dp[i]=2
+        else: dp[i]=(dp[i-1]+dp[i-2])%1234567
+    return dp[n]
